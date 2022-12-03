8e8a47b37aa48cccbbd2fcf117b4c139e8e7687d
sajid365-sr <sajidsorker86@gmail.com>
2022-12-03 22:16:48 +0600

    mongodb setup complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -12,6 +13,27 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Book Keeper server is running')
 })
+
+// MongoDB
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.90qadcl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const run = async() =>{
+
+    try{
+console.log(uri)
+    }
+    catch{
+
+    }
+}
+
+
+run().catch(err => console.error(err));
+
 
 // Listen
 app.listen(port, () =>{
