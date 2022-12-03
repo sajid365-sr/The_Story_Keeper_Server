acc68864d71b3982c1e5388a0e257de1df020b23
sajid365-sr <sajidsorker86@gmail.com>
2022-12-03 17:01:01 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+// Default route
+app.get('/', (req, res) => {
+    res.send('Book Keeper server is running')
+})
+
+// Listen
+app.listen(port, () =>{
+    console.log(`Book Keeper server is running on port: ${port}`);
+})
