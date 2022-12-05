2fbf49b8c3522eb14d185d14ff888baa30569a7c
sajid365-sr <sajidsorker86@gmail.com>
2022-12-05 15:49:40 +0600

    get all book route, specefic categroy route complete

--- index.js
+++ index.js
@@ -1,42 +1,102 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config();
+require("dotenv").config();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Default route
-app.get('/', (req, res) => {
-    res.send('Book Keeper server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Book Keeper server is running");
+});
 
-// MongoDB
-
-
+// MongoDB connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.90qadcl.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+const run = async () => {
+  const BooksCollection = client.db("TheStoryKeeper").collection("Books");
 
-const run = async() =>{
+  try {
+    // Get some category book to display home page
+    app.get("/books", async (req, res) => {
+        const result = await BooksCollection.find({}).toArray();
+    //   const result = require("./books.json");
 
-    try{
-console.log(uri)
-    }
-    catch{
+      // Find all category by Category Id
+      const category = [];
+      const categories = result.map((book) => book.categoryId);
+      categories.forEach((id) => {
+        if (!category.includes(id)) {
+          category.push(id);
+        }
+      });
+      const sliceCategory = category.slice(0, 2); // Slice 3 category for display home page
 
-    }
-}
+      // Filter all books by category Id
+      const filteredBooks = [];
+      sliceCategory.forEach((catId) => {
+        const bookByCatId = result.filter((book) => book.categoryId === catId);
+        filteredBooks.push(bookByCatId);
+      });
 
+      const sliceFilteredBook = filteredBooks.slice(0, 4); // Slice only 4 books to display home page
 
-run().catch(err => console.error(err));
+      res.send(sliceFilteredBook);
+    });
 
+    // Get specific category books by Id
+    app.get("/category/:id", async (req, res) => {
+      const catId = req.params.id;
+      const query = { categoryId: catId };
+      const result = await BooksCollection.find(query).toArray();
+
+    //   const test = require("./books.json");
+    //   const result = test.filter((book) => book.categoryId === catId);
+
+      res.send(result);
+    });
+
+    // Get all books
+    app.get("/allBooks", async (req, res) => {
+      const query = {};
+      const result = await BooksCollection.find(query).toArray();
+
+    //   const result = require("./books.json");
+
+      // Filter all specific category id
+      const category = [];
+      const categories = result.map((book) => book.categoryId);
+      categories.forEach((id) => {
+        if (!category.includes(id)) {
+          category.push(id);
+        }
+      });
+
+      // Filter all books by category Id
+      const filteredBooks = [];
+      category.forEach((catId) => {
+        const bookByCatId = result.filter((book) => book.categoryId === catId);
+        filteredBooks.push(bookByCatId);
+      });
+      const sliceFilteredBook = filteredBooks.slice(0, 8);
+
+      res.send(sliceFilteredBook);
+    });
+  } catch {}
+};
+
+run().catch((err) => console.error(err));
 
 // Listen
-app.listen(port, () =>{
-    console.log(`Book Keeper server is running on port: ${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`Book Keeper server is running on port: ${port}`);
+});
